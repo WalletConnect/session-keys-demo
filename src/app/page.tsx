9d2e3ee8df69358549fe5cdbbc68234dc8ba4c9a
Luka Isailovic <lukaisailovic@gmail.com>
2024-03-05 15:23:18 +0100

    chore: styling update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,8 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function Home() {
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Session keys demo</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <PasskeySection />
-        </CardContent>
-      </Card>
+    <div className="w-3/4 sm:w-1/2 md:w-1/4 flex flex-col gap-3">
+      <PasskeySection />
     </div>
   )
 }
--- src/components/sections/passkey.tsx
+++ src/components/sections/passkey.tsx
@@ -3,6 +3,7 @@ import usePasskey from '@/hooks/usePasskey'
 import { Button } from '../ui/button'
 import { createPasskey, signWithPasskey, toLocalStorageFormat } from '@/core/passkeys'
 import { bufferToString } from '@/lib/utils'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 
 export default function PasskeySection() {
   const { isPasskeyAvailable, setPasskey } = usePasskey()
@@ -35,14 +36,25 @@ export default function PasskeySection() {
     setPasskey(undefined)
   }
 
-  return isPasskeyAvailable ? (
-    <div className="flex flex-col gap-2">
-      <Button onClick={handleSign}>Sign with passkey</Button>
-      <Button onClick={handleClear} variant="secondary">
-        Clear passkey
-      </Button>
-    </div>
-  ) : (
-    <Button onClick={handleCreatePasskey}>Create Passkey</Button>
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Passkey management</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {isPasskeyAvailable ? (
+            <div className="flex flex-col gap-2">
+              <Button onClick={handleSign}>Sign with passkey</Button>
+              <Button onClick={handleClear} variant="secondary">
+                Clear passkey
+              </Button>
+            </div>
+          ) : (
+            <Button onClick={handleCreatePasskey}>Create Passkey</Button>
+          )}
+        </CardContent>
+      </Card>
+    </>
   )
 }
